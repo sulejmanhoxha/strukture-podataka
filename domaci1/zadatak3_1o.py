e5ebf634a03b483978542af1014e6d87ddbe297c
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2025-04-13 15:59:42 +0200

    feat: 1o - broj neparnih

---
+++ domaci1/zadatak3_1o.py
@@ -0,0 +1,51 @@
+def count_odd_digits_recursive(n: int) -> int:
+    """
+    Rekurzivna funkcija koja prebrojava neparni brojeva u zadatom broju.
+
+    Args:
+        n (int): Broj ciji se neparni brojevi broje
+
+    Returns:
+        int: Broj neparnih brojeva u zadatom broju
+    """
+    # Bazni slucaj: Ako je broj manji od 10, provjeri je li neparni
+    if n < 10:
+        return 1 if n % 2 != 0 else 0
+
+    # Rekurzivni slucaj: Broj neparnih brojeva je 1 + broj neparnih brojeva ostatka broja
+    return (1 if n % 2 != 0 else 0) + count_odd_digits_recursive(n // 10)
+
+
+numbers_to_test = [
+    4,
+    12,
+    123,
+    1234,
+    12345,
+    123456,
+    1234567,
+    12345678,
+    123456789,
+    1234567890,
+]
+
+expected_results = {
+    4: 0,
+    12: 1,
+    123: 2,
+    1234: 2,
+    12345: 3,
+    123456: 3,
+    1234567: 4,
+    12345678: 4,
+    123456789: 5,
+    1234567890: 5,
+    4365783464: 4,
+}
+
+for n, expected in expected_results.items():
+    result = count_odd_digits_recursive(n)
+    if result == expected:
+        print(f"{n} ima {result} neparnih brojeva ✓")
+    else:
+        print(f"{n} ima {result} neparnih brojeva ✗ (Očekivano: {expected})")
