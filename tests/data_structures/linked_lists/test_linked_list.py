13172bb23220df1b8488e5eeaab1d06469926680
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2025-06-18 23:19:27 +0200

    add blank test files

---
+++ tests/data_structures/linked_lists/test_linked_list.py
@@ -0,0 +1,36 @@
+from data_structures.linked_lists.linked_list import LinkedList, Node
+
+
+def test_append():
+    l1 = LinkedList()
+    l1.append(Node(1))
+    l1.append(Node(2))
+    l1.append(Node(3))
+    l1.append(Node(4))
+    l1.append(Node(5))
+
+    assert l1.length_iterative() == 5
+    assert l1.length_recursive(l1.head) == 5
+
+
+def test_prepend():
+    l1 = LinkedList()
+    l1.prepend(Node(1))
+    l1.prepend(Node(2))
+    l1.prepend(Node(3))
+    l1.prepend(Node(4))
+    l1.prepend(Node(5))
+
+    assert l1.length_iterative() == 5
+    assert l1.length_recursive(l1.head) == 5
+
+
+def test_find_maximum_value():
+    l1 = LinkedList()
+    l1.append(Node(1))
+    l1.append(Node(2))
+    l1.append(Node(3))
+    l1.append(Node(4))
+    l1.append(Node(5))
+
+    assert l1.find_maximum_value() == 5
